2ca7a5cbc9f8350caacb39e60ff7ce53771bb287
Beatrice <72189675+f0ffee@users.noreply.github.com>
2020-10-03 22:27:14 +0200

    use search API

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function sendRequest(url, callback) {
         callback(result);
     });
     httpRequest.open('GET', url);
+    httpRequest.setRequestHeader('Accept', 'application/vnd.github.cloak-preview');
     httpRequest.setRequestHeader('Authorization', 'Basic ZjBmZmVlOjIwMWM3ZGZmZTI1YzdmNmMwMjIzMzYwZWYxN2UwNzA2MmZhMDE3ODk=');
     httpRequest.send();
 }
@@ -15,42 +16,17 @@ document.getElementById('username').addEventListener('input', event => {
     const username = event.target.value;
     if (!checkname.test(username)) return;
 
-    let userid;
-    const yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - 1);
-    let commits = [];
+    const d = new Date();
+    const today = d.getFullYear() + '-' + ('0' + (d.getMonth() + 1)).substr(-2) + '-' + ('0' + d.getDate()).substr(-2);
 
-    function handleUserResponse(result) {
-        userid = result.id;
-        sendRequest('https://api.github.com/users/' + result.login + '/events', handleEventsResponse);
-    }
+    sendRequest('https://api.github.com/search/commits?q=author:' + username + '+author-date:' + today, handleResponse);
 
-    function handleEventsResponse(result) {
-        result.forEach(element => {
-            if (element.type !== 'PushEvent') return;
-
-            const elementDate = new Date(element.created_at);
-            if (elementDate < yesterday) return;
-
-            element.payload.commits.forEach(commit => {
-                sendRequest(commit.url, handleCommitResponse);
-            });
-        });
-    }
-
-    function handleCommitResponse(result) {
-        if (!result.author || result.author.id !== userid) return;
-
-        const elementDate = new Date(result.commit.author.date);
-        if (elementDate < yesterday) return;
-
-        if (commits.length === 0) {
+    function handleResponse(result) {
+        if (result.total_count !== 0) {
             document.getElementById('output').innerHTML = 'yes';
         }
-
-        commits.push(result);
+        else {
+            document.getElementById('output').innerHTML = 'no';
+        }
     }
-
-    document.getElementById('output').innerHTML = 'no';
-    sendRequest('https://api.github.com/users/' + username, handleUserResponse);
 });
